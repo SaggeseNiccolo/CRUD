900172a1c59c376a998a2e6ab580b24667197bd0
saggeseniccolo <snicco2004@gmail.com>
2022-02-24 20:09:23 +0100

    aggiunto modifica

--- script.js
+++ script.js
@@ -45,13 +45,12 @@ var data = [
 
 var id;
 var nextId = 10006;
-var btnModifica = "<button class='btn btn-primary me-5 modifica' data-bs-toggle='modal' data-bs-target='#modal-modify'>Modifica</button>";
+var btnModifica = "<button class='btn btn-primary ms-5 modifica' data-bs-toggle='modal' data-bs-target='#modal-modify'>Modifica</button>";
 var btnElimina = "<button class='btn btn-danger elimina'>Elimina</button>";
 
 //una volta che la pagina viene caricata, vengono inseriti gli elementi nella tabella
 $(document).ready(
     displayTable(),
-    elimina()
 );
 
 function displayTable() {
@@ -64,7 +63,7 @@ function displayTable() {
         dipendente += '<th scope="row">' + value.id + '</th>';
         dipendente += '<td>' + value.firstName + '</td>';
         dipendente += '<td>' + value.lastName + '</td>';
-        dipendente += '<td data-id=' + value.id + '>' + btnModifica + btnElimina + '</td>';
+        dipendente += '<td data-id=' + value.id + '>' + btnElimina + btnModifica + '</td>';
         dipendente += '</tr>';
     });
     $("tbody").append(dipendente);
@@ -91,7 +90,18 @@ function displayTable() {
             }
         }
         displayTable();
-        elimina();
+    });
+
+    $(".elimina").click(function () {
+        $(this).parents("tr").fadeOut("fast");
+
+        var id = $(this).parent().data("id");
+
+        for (var i = 0; i < data.length; i++) {
+            if (id == data[i].id) {
+                data.splice(i, 1);
+            }
+        }
     });
 }
 
@@ -118,19 +128,4 @@ $("#aggiungi").click(function () {
     nextId++;
 
     displayTable();
-    elimina();
 });
-
-function elimina() {
-    $(".elimina").click(function () {
-        $(this).parents("tr").fadeOut("fast");
-
-        var id = $(this).parent().data("id");
-
-        for (var i = 0; i < data.length; i++) {
-            if (id == data[i].id) {
-                data.splice(i, 1);
-            }
-        }
-    });
-}
