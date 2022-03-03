a50c2dca3db28700bd59df40809f370e17dcfed1
saggeseniccolo <snicco2004@gmail.com>
2022-03-03 16:31:53 +0100

    paginazione funzionante

--- script.js
+++ script.js
@@ -1,59 +1,51 @@
-var data = [
-    {
-        "id": 10001,
-        "birthDate": "1953-09-01",
-        "firstName": "Georgi",
-        "lastName": "Facello",
-        "gender": "M",
-        "hireDate": "1986-06-25",
-    },
-    {
-        "id": 10002,
-        "birthDate": "1964-06-01",
-        "firstName": "Bezalel",
-        "lastName": "Simmel",
-        "gender": "F",
-        "hireDate": "1985-11-20",
-    },
-    {
-        "id": 10003,
-        "birthDate": "1959-12-02",
-        "firstName": "Parto",
-        "lastName": "Bamford",
-        "gender": "M",
-        "hireDate": "1986-08-27T22:00:00.000+0000",
-    },
-    {
-        "id": 10004,
-        "birthDate": "1954-04-30",
-        "firstName": "Chirstian",
-        "lastName": "Koblick",
-        "gender": "M",
-        "hireDate": "1986-11-30",
-
-    },
-    {
-        "id": 10005,
-        "birthDate": "1955-01-20",
-        "firstName": "Kyoichi",
-        "lastName": "Maliniak",
-        "gender": "M",
-        "hireDate": "1989-09-11T22:00:00.000+0000",
-
-    }
-]
-
+var page;
+var first;
+var last;
+var prev;
+var next;
 var id;
 var nextId = 10006;
 var btnModifica = "<button class='btn btn-primary ms-5 modifica' data-bs-toggle='modal' data-bs-target='#modal-modify'>Modifica</button>";
 var btnElimina = "<button class='btn btn-danger elimina'>Elimina</button>";
 
+function nPage() {
+    $("#current-page").html(page);
+    if (page == 0) {
+        $("#first").parent().addClass("disabled");
+        $("#prev").parent().addClass("disabled");
+        $("#next").parent().removeClass("disabled");
+        $("#last").parent().removeClass("disabled");
+    } else if (page == 30002){
+        $("#first").parent().removeClass("disabled");
+        $("#prev").parent().removeClass("disabled");
+        $("#next").parent().addClass("disabled");
+        $("#last").parent().addClass("disabled");
+    } else {
+        $("#first").parent().removeClass("disabled");
+        $("#prev").parent().removeClass("disabled");
+        $("#next").parent().removeClass("disabled");
+        $("#last").parent().removeClass("disabled");
+    }
+}
+
 //una volta che la pagina viene caricata, vengono inseriti gli elementi nella tabella
 $(document).ready(
-    displayTable(),
+    $.get("http://localhost:8080/employees?size=10",
+        function (data) {
+            page = data["page"]["number"];
+            next = data["_links"]["next"]["href"];
+            if (page > 0) {
+                prev = data["_links"]["prev"]["href"];
+            }
+            first = data["_links"]["first"]["href"];
+            last = data["_links"]["last"]["href"];
+            displayTable(data['_embedded']['employees']);
+            nPage();
+        },
+    )
 );
 
-function displayTable() {
+function displayTable(data) {
     var dipendente;
 
     $("tbody").html("");
@@ -67,43 +59,64 @@ function displayTable() {
         dipendente += '</tr>';
     });
     $("tbody").append(dipendente);
-
-    $(".modifica").click(function () {
-        id = $(this).parent().data("id");
-
-        for (var i = 0; i < data.length; i++) {
-            if (id == data[i].id) {
-                $("#nome-m").val(data[i].firstName);
-                $("#cognome-m").val(data[i].lastName);
-            }
-        }
-    });
-
-    $("#modifica").click(function () {
-        var nome = $("#nome-m").val();
-        var cognome = $("#cognome-m").val();
-
-        for (var i = 0; i < data.length; i++) {
-            if (id == data[i].id) {
-                data[i].firstName = nome;
-                data[i].lastName = cognome;
-            }
-        }
-        displayTable();
-    });
-
-    $(".elimina").click(function () {
-        $(this).parents("tr").fadeOut("fast");
-
-        var id = $(this).parent().data("id");
-
-        for (var i = 0; i < data.length; i++) {
-            if (id == data[i].id) {
-                data.splice(i, 1);
-            }
-        }
-    });
 }
+
+$("#next").click(function () {
+    $.get(next,
+        function (data) {
+            displayTable(data['_embedded']['employees']);
+            page = data["page"]["number"];
+            if(page < 30002)
+            next = data["_links"]["next"]["href"];
+            prev = data["_links"]["prev"]["href"];
+            nPage();
+        },
+    );
+});
+
+$("#prev").click(function () {
+    $.get(prev,
+        function (data) {
+            displayTable(data['_embedded']['employees']);
+            page = data["page"]["number"];
+            next = data["_links"]["next"]["href"];
+            if (page > 0) {
+                prev = data["_links"]["prev"]["href"];
+            }
+            nPage();
+        },
+    );
+});
+
+$("#first").click(function () {
+    $.get(first,
+        function (data) {
+            displayTable(data['_embedded']['employees']);
+            page = data["page"]["number"];
+            next = data["_links"]["next"]["href"];
+            nPage();
+            $("#first").parent().addClass("disabled");
+            $("#prev").parent().addClass("disabled");
+            $("#next").parent().removeClass("disabled");
+            $("#last").parent().removeClass("disabled");
+        },
+    );
+});
+
+$("#last").click(function () {
+    $.get(last,
+        function (data) {
+            displayTable(data['_embedded']['employees']);
+            page = data["page"]["number"];
+            prev = data["_links"]["prev"]["href"];
+            nPage();
+            $("#first").parent().removeClass("disabled");
+            $("#prev").parent().removeClass("disabled");
+            $("#next").parent().addClass("disabled");
+            $("#last").parent().addClass("disabled");
+        },
+    );
+});
 
 $("#aggiungi").click(function () {
     var nome = $("#nome").val();
@@ -128,4 +141,40 @@ $("#aggiungi").click(function () {
     nextId++;
 
     displayTable();
+});
+
+$("#modifica").click(function () {
+    var nome = $("#nome-m").val();
+    var cognome = $("#cognome-m").val();
+
+    for (var i = 0; i < data.length; i++) {
+        if (id == data[i].id) {
+            data[i].firstName = nome;
+            data[i].lastName = cognome;
+        }
+    }
+    displayTable();
+});
+
+$(".modifica").click(function () {
+    id = $(this).parent().data("id");
+
+    for (var i = 0; i < data.length; i++) {
+        if (id == data[i].id) {
+            $("#nome-m").val(data[i].firstName);
+            $("#cognome-m").val(data[i].lastName);
+        }
+    }
+});
+
+$(".elimina").click(function () {
+    $(this).parents("tr").fadeOut("fast");
+
+    var id = $(this).parent().data("id");
+
+    for (var i = 0; i < data.length; i++) {
+        if (id == data[i].id) {
+            data.splice(i, 1);
+        }
+    }
 });
